248a5adb420a40405a32d0a6a6d697bdf8300436
36121YoshiedaKai <oibc36121@ojs.ac.jp>
2025-04-23 10:34:19 +0900

    機能追加②完成

--- Chapter02/演習問題/Exercise02/Program.cs
+++ Chapter02/演習問題/Exercise02/Program.cs
@@ -1,14 +1,23 @@
-﻿namespace Exercise02 {
+﻿using System.Diagnostics.Metrics;
+using System.Threading;
+
+namespace Exercise02 {
     internal class Program {
         static void Main(string[] args) {
 
+
             //コンソール入力
-            Console.WriteLine("１：インチからメートル");
-            Console.WriteLine("２：メートルからインチ");
+            /* Console.WriteLine("１：インチからメートル");
+             Console.WriteLine("２：メートルからインチ");*/
+            Console.WriteLine("１：ヤードからメートル");
+            Console.WriteLine("２：メートルからヤード");
             Console.Write("＞");
             int oneortwo = int.Parse(Console.ReadLine());
 
-            Console.Write("はじめ：");
+            Console.Write("変換する数字：");
+            int convertno = int.Parse(Console.ReadLine());
+
+            /*Console.Write("はじめ：");
             int start = int.Parse(Console.ReadLine());
 
             Console.Write("おわり：");
@@ -16,8 +25,16 @@
 
             if (oneortwo == 1) {
                 PrintInchToMeter(start, end);
-            }else if(oneortwo == 2) {
+            } else if (oneortwo == 2) {
                 PrintMeterToInch(start, end);
+            } else {
+                Console.WriteLine("エラー");
+            }*/
+
+            if (oneortwo == 1) {
+                PrintYardToMeter(convertno);
+            } else if (oneortwo == 2) {
+                PrintMeterToYard(convertno);
             } else {
                 Console.WriteLine("エラー");
             }
@@ -38,6 +55,30 @@
                 double Inch = InchConverter.FromMeter(meter);
                 Console.WriteLine($"{meter}m = {Inch:0.0000}inch");
             }
+        }
+
+
+        //ヤードからメートル
+        static void PrintYardToMeter(int Yard) {
+            /* for (int Yard = strat; Yard <= end; Yard++) {
+                 double meter = YardConverter.ToYard(Yard);
+                 Console.WriteLine($"{Yard}yard = {meter:0.0000}m");
+             }*/
+            double meter = YardConverter.ToYard(Yard);
+            Console.WriteLine($"変更前（ヤード）：{Yard}yard ");
+            Console.WriteLine($"変更後（メートル）：{ meter: 0.0000}m");
+
+        }
+
+        //メートルからヤード
+        static void PrintMeterToYard(int meter) {
+            /*for (int meter = strat; meter <= end; meter++) {
+                double Yard = YardConverter.FromMeter(meter);
+                Console.WriteLine($"{meter}m = {Yard:0.0000}yard");
+            }*/
+            double Yard = YardConverter.FromMeter(meter);
+            Console.WriteLine($"変更前（メートル）：{meter}m ");
+            Console.WriteLine($"変更後（ヤード）：{Yard: 0.0000}yard");
         }
     }
 }
---
+++ Chapter02/演習問題/Exercise02/YardConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class YardConverter {
+
+        //定数
+        private const double ratio = 0.9144;
+
+        //ヤードからメートルへ計算
+        public static double ToYard(double Yard) {
+            return Yard * ratio;
+        }
+
+        //メートルからヤードを求める
+        public static double FromMeter(double meter) {
+            return meter / ratio;
+        }
+
+    }
+}
