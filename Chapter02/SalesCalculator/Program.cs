0af38abb209ec1f4523767f0473681f50be673a5
36121YoshiedaKai <oibc36121@ojs.ac.jp>
2025-05-09 15:50:00 +0900

    インターフェイスへの置き換え

--- Chapter02/SalesCalculator/Program.cs
+++ Chapter02/SalesCalculator/Program.cs
@@ -2,15 +2,10 @@
     internal class Program {
         static void Main(string[] args) {
             SalesCounter salse = new SalesCounter(@"data\sales.csv");
-            Dictionary<string, int> amountsPerStore = salse.GetParStoreSales();
+            IDictionary<string, int> amountsPerStore = salse.GetParStoreSales();
             foreach (KeyValuePair<string, int> obj in amountsPerStore) {
                 Console.WriteLine($"{obj.Key}{obj.Value}");
             }
-
-
-
         }
-
-
     }
 }
