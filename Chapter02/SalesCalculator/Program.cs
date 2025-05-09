267d96293622c175562b7171274d9feb6e4314fe
36121YoshiedaKai <oibc36121@ojs.ac.jp>
2025-05-09 15:59:34 +0900

    暗黙の型指定var

--- Chapter02/SalesCalculator/Program.cs
+++ Chapter02/SalesCalculator/Program.cs
@@ -1,9 +1,9 @@
 ﻿namespace SalesCalculator {
     internal class Program {
         static void Main(string[] args) {
-            SalesCounter salse = new SalesCounter(@"data\sales.csv");
-            IDictionary<string, int> amountsPerStore = salse.GetParStoreSales();
-            foreach (KeyValuePair<string, int> obj in amountsPerStore) {
+            var salse = new SalesCounter(@"data\sales.csv");
+            var amountsPerStore = salse.GetParStoreSales();
+            foreach (var obj in amountsPerStore) {
                 Console.WriteLine($"{obj.Key}{obj.Value}");
             }
         }
