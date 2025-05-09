83f5419e992f235110850efa608c9ef03e9a0962
36121YoshiedaKai <oibc36121@ojs.ac.jp>
2025-05-09 14:56:52 +0900

    メソッドの移動

--- Chapter02/SalesCalculator/Program.cs
+++ Chapter02/SalesCalculator/Program.cs
@@ -1,36 +1,16 @@
 ﻿namespace SalesCalculator {
     internal class Program {
         static void Main(string[] args) {
-            SalesCounter salse = new SalesCounter(ReadSales(@"data\sales.csv"));
+            SalesCounter salse = new SalesCounter(SalesCounter.ReadSales(@"data\sales.csv"));
             Dictionary<string, int> amountsPerStore = salse.GetParStoreSales();
             foreach (KeyValuePair<string, int> obj in amountsPerStore) {
                 Console.WriteLine($"{obj.Key}{obj.Value}");
             }
 
 
-        }
-
-        //売り上げデータを読み込み、Saleオブジェクトのリストを返す
-        static List<Sale> ReadSales(string filePath) {
-            //売り上げデータを入れるリストオブジェクトを生成
-            List<Sale> sales = new List<Sale>();
-            //ファイルを一気に読み込む
-            string[] lines = File.ReadAllLines(filePath);
-            //読み込んだ行数分繰り返し
-            foreach (string line in lines) {
-                string[] items = line.Split(',');
-                //Saleオブジェクトを生成
-                Sale sale = new Sale() {
-                    ShopName = items[0],
-                    ProductCategory = items[1],
-                    Amount = int.Parse(items[2])
-                };
-                sales.Add(sale);
-
-            }
-
-            return sales;
 
         }
+
+
     }
 }
