f80251b057e8a3d825648ac8a42d311f4723f492
36121YoshiedaKai <oibc36121@ojs.ac.jp>
2025-05-13 10:03:33 +0900

    2.1.1完成

---
+++ Chapter02/演習問題/Exercise01/Song.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    //2.1.1
+    public class Song {
+
+        public string Title { get; set; } = string.Empty;
+        string ArtistName { get; set; } = string.Empty;
+        int Length;
+    }
+    //2.1.2
+
+
+}
