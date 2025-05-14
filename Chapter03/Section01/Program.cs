50ed7660c1b5a08f447536e68cd1b766a4d18375
36121YoshiedaKai <oibc36121@ojs.ac.jp>
2025-05-14 11:42:36 +0900

    コード3.1完成

---
+++ Chapter03/Section01/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.Write("カウントしたい数値：");
+            int Num = int.Parse(Console.ReadLine());
+            Console.WriteLine(Count(Num));
+
+
+
+        }
+
+        static int Count(int num) {
+            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
+            var count = 0;
+            foreach (var n in numbers) {
+                if(n == num) {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+    }
+}
