60b10d72c097e1ae517db1f8c521fe7b56f26f29
36121YoshiedaKai <oibc36121@ojs.ac.jp>
2025-05-23 14:44:35 +0900

    4.1完成

--- Chapter04/演習問題/Exercise01/Program.cs
+++ Chapter04/演習問題/Exercise01/Program.cs
@@ -17,15 +17,80 @@ namespace Exercise01 {
         }
 
         private static void Exercise1(List<string> langs) {
-            
+            //foreach文
+            foreach (var s in langs) {
+                if (s.Contains('S'))
+                    Console.WriteLine(s);
+            }
+
+            Console.WriteLine();
+
+            //for文
+            for (var i = 0; i < langs.Count; i++) {
+                if (langs[i].Contains('S'))
+                    Console.WriteLine(langs[i]);
+            }
+
+            Console.WriteLine();
+
+            //while文
+            var index = 0;
+            while (index < langs.Count) {
+                if (langs[index].Contains('S'))
+                    Console.WriteLine(langs[index]);
+                index++;
+
+
+            }
         }
 
         private static void Exercise2(List<string> langs) {
-            
+            var index = langs.Where(s => s.Contains('S'));
+            foreach (var lang in index) {
+                Console.WriteLine(lang);
+            }
         }
 
         private static void Exercise3(List<string> langs) {
-            
+
+            var getfind = langs.Find(s => s.Length == 10)?? "unknown";
+            Console.WriteLine(getfind);
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
