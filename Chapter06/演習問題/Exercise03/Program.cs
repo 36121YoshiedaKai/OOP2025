2588e74f002704b6a303bdb9d8f40986d1fd31a0
36121YoshiedaKai <oibc36121@ojs.ac.jp>
2025-05-30 15:36:47 +0900

    1,2完成

---
+++ Chapter06/演習問題/Exercise03/Program.cs
@@ -0,0 +1,50 @@
+﻿
+using System.Diagnostics.Metrics;
+using System.Reflection;
+
+namespace Exercise03 {
+    internal class Program {
+        static void Main(string[] args) {
+            var text = "Jackdaws love my big sphinx of quartz";
+
+            Console.WriteLine("6.3.1");
+            Exercise1(text);
+
+            Console.WriteLine("6.3.2");
+            Exercise2(text);
+
+            Console.WriteLine("6.3.3");
+            Exercise3(text);
+
+            Console.WriteLine("6.3.4");
+            Exercise4(text);
+
+            Console.WriteLine("6.3.5");
+            Exercise5(text);
+
+        }
+
+        private static void Exercise1(string text) {
+            Console.Write("空白は");
+            //Console.WriteLine(text.Count(c => c == ' '));
+            Console.WriteLine(text.Count(char.IsWhiteSpace));
+
+        }
+
+        private static void Exercise2(string text) {
+            Console.WriteLine(text.Replace("big", "small"));
+        }
+
+        private static void Exercise3(string text) {
+            
+        }
+
+        private static void Exercise4(string text) {
+            Console.WriteLine(text.Count());
+        }
+
+        private static void Exercise5(string text) {
+            
+        }
+    }
+}
