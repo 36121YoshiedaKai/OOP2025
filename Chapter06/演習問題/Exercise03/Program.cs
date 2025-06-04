7ea33c156cecda1739451d71e5b853576417baf1
36121YoshiedaKai <oibc36121@ojs.ac.jp>
2025-06-04 11:31:10 +0900

    99完成

--- Chapter06/演習問題/Exercise03/Program.cs
+++ Chapter06/演習問題/Exercise03/Program.cs
@@ -73,99 +73,130 @@ namespace Exercise03 {
         }
 
         private static void Exercise6(string text) {
-            var atoz = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, };
+            //var atoz = new int[26];
+            //var smalltext = text.ToLower().Replace(" ", "");
+            //foreach (var c in smalltext) {
+            //    switch (c) {
+            //        case 'a':
+            //            atoz[0] += 1;
+            //            break;
+            //        case 'b':
+            //            atoz[1] += 1;
+            //            break;
+            //        case 'c':
+            //            atoz[2] += 1;
+            //            break;
+            //        case 'd':
+            //            atoz[3] += 1;
+            //            break;
+            //        case 'e':
+            //            atoz[4] += 1;
+            //            break;
+            //        case 'f':
+            //            atoz[5] += 1;
+            //            break;
+            //        case 'g':
+            //            atoz[6] += 1;
+            //            break;
+            //        case 'h':
+            //            atoz[7] += 1;
+            //            break;
+            //        case 'i':
+            //            atoz[8] += 1;
+            //            break;
+            //        case 'j':
+            //            atoz[9] += 1;
+            //            break;
+            //        case 'k':
+            //            atoz[10] += 1;
+            //            break;
+            //        case 'l':
+            //            atoz[11] += 1;
+            //            break;
+            //        case 'm':
+            //            atoz[12] += 1;
+            //            break;
+            //        case 'n':
+            //            atoz[13] += 1;
+            //            break;
+            //        case 'o':
+            //            atoz[14] += 1;
+            //            break;
+            //        case 'p':
+            //            atoz[15] += 1;
+            //            break;
+            //        case 'q':
+            //            atoz[16] += 1;
+            //            break;
+            //        case 'r':
+            //            atoz[17] += 1;
+            //            break;
+            //        case 's':
+            //            atoz[18] += 1;
+            //            break;
+            //        case 't':
+            //            atoz[19] += 1;
+            //            break;
+            //        case 'u':
+            //            atoz[20] += 1;
+            //            break;
+            //        case 'v':
+            //            atoz[21] += 1;
+            //            break;
+            //        case 'w':
+            //            atoz[22] += 1;
+            //            break;
+            //        case 'x':
+            //            atoz[23] += 1;
+            //            break;
+            //        case 'y':
+            //            atoz[24] += 1;
+            //            break;
+            //        case 'z':
+            //            atoz[25] += 1;
+            //            break;
+            //        default:
+            //            Console.WriteLine("エラー");
+            //            break;
+            //    }
+            //}
+            //char az = 'a';
+            //for (int i = 0; i < 26; i++) {
+            //    Console.WriteLine((az) + ":" + atoz[i]);
+            //    az++;
+            //}
+
+
+            var atoz = new int[26];
             var smalltext = text.ToLower().Replace(" ", "");
+
             foreach (var c in smalltext) {
-                switch (c) {
-                    case 'a':
-                        atoz[0] += 1;
-                        break;
-                    case 'b':
-                        atoz[1] += 1;
-                        break;
-                    case 'c':
-                        atoz[2] += 1;
-                        break;
-                    case 'd':
-                        atoz[3] += 1;
-                        break;
-                    case 'e':
-                        atoz[4] += 1;
-                        break;
-                    case 'f':
-                        atoz[5] += 1;
-                        break;
-                    case 'g':
-                        atoz[6] += 1;
-                        break;
-                    case 'h':
-                        atoz[7] += 1;
-                        break;
-                    case 'i':
-                        atoz[8] += 1;
-                        break;
-                    case 'j':
-                        atoz[9] += 1;
-                        break;
-                    case 'k':
-                        atoz[10] += 1;
-                        break;
-                    case 'l':
-                        atoz[11] += 1;
-                        break;
-                    case 'm':
-                        atoz[12] += 1;
-                        break;
-                    case 'n':
-                        atoz[13] += 1;
-                        break;
-                    case 'o':
-                        atoz[14] += 1;
-                        break;
-                    case 'p':
-                        atoz[15] += 1;
-                        break;
-                    case 'q':
-                        atoz[16] += 1;
-                        break;
-                    case 'r':
-                        atoz[17] += 1;
-                        break;
-                    case 's':
-                        atoz[18] += 1;
-                        break;
-                    case 't':
-                        atoz[19] += 1;
-                        break;
-                    case 'u':
-                        atoz[20] += 1;
-                        break;
-                    case 'v':
-                        atoz[21] += 1;
-                        break;
-                    case 'w':
-                        atoz[22] += 1;
-                        break;
-                    case 'x':
-                        atoz[23] += 1;
-                        break;
-                    case 'y':
-                        atoz[24] += 1;
-                        break;
-                    case 'z':
-                        atoz[25] += 1;
-                        break;
-                    default:
-                        Console.WriteLine("エラー");
-                        break;
-                }
+                if (c >= 'a' && c <= 'z')
+                    atoz[c - 'a']++;
+                else
+                    Console.WriteLine("エラー");
             }
-            char az = 'a';
+
             for (int i = 0; i < 26; i++) {
-                Console.WriteLine((az)+ ":" + atoz[i]);
-                az++;
+                Console.WriteLine($"{(char)('a' + i)}: {atoz[i]}");
             }
-                
+
+            //辞書を使う
+            //var smalltext = text.ToLower().Replace(" ", "");
+            //var alphDicCount = Enumerable.Range('a', 26)
+            //    .ToDictionary(n =>((char) n).ToString(),n => 0);
+            //foreach (var alph in smalltext) {
+            //    alphDicCount[alph.ToString()]++;
+            //}
+            //foreach (var item in alphDicCount) {
+            //    Console.WriteLine($"{item.Key}:{item.Value}");
+
+            //}
+
+            //aから順にカウントして出力
+            //for (char ch = 'a'; ch <= 'z'; ch++) {
+            //    Console.WriteLine($"{ch}:{text.Count(tc => tc == ch)}");
+            //}
         }
     }
 
