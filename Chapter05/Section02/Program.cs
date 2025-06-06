d6d883d5d09cbec792aef81b04cfed2a8d005377
36121YoshiedaKai <oibc36121@ojs.ac.jp>
2025-06-06 14:27:30 +0900

    演習問題開始

---
+++ Chapter05/Section02/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Section02 {
+    internal class Program {
+        static void Main(string[] args) {
+            var appVer1 = new AppVersion(5, 1);
+            var appVer2 = new AppVersion(5, 1);
+
+            if (appVer1 == appVer2) {
+                Console.WriteLine("等しい");
+            } else {
+                Console.WriteLine("等しくない");
+            }
+        }
+    }
+
+    //プライマリーコンストラクタを使ったクラスの定義
+    public /*class*/record AppVersion(int m, int mi, int b = 0, int r = 0) {
+        public int Major { get; init; } = m;
+        public int Minor { get; init; } = mi;
+        public int Build { get; init; } = b;
+        public int Revision { get; init; } = r;
+
+        //public AppVersion(int major, int minor)
+        //    : this(major, minor, 0, 0) {  //- 引数4つのコンストラクターを呼び出す
+        //}
+
+        //public AppVersion(int major, int minor, int build)
+        //    : this(major, minor, build, 0) {  //- 引数4つのコンストラクターを呼び出す
+        //}
+
+        //public AppVersion(int major, int minor, int build = 0, int revision = 0) {
+        //    Major = major;
+        //    Minor = minor;
+        //    Build = build;
+        //    Revision = revision;
+        //}
+
+        public override string ToString() =>
+            $"{Major}.{Minor}.{Build}.{Revision}";
+
+    }
+}
