42a67e3f007c99244913a1774237acc24a563732
36121YoshiedaKai <oibc36121@ojs.ac.jp>
2025-06-06 15:40:05 +0900

    一応完成

--- Chapter05/演習問題/Exercise01/Program.cs
+++ Chapter05/演習問題/Exercise01/Program.cs
@@ -1,7 +1,10 @@
 ﻿namespace Exercise01 {
     internal class Program {
         static void Main(string[] args) {
-            
+            var ym = new YearMonth(2100,2);
+            Console.WriteLine(ym.Is21Century);
+            Console.WriteLine(ym.AddOneMonth());
+            Console.WriteLine(ym.ToString());
         }
     }
 }
---
+++ Chapter05/演習問題/Exercise01/YearMonth.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    // 5.1.1
+    public class YearMonth {
+        public int Year { get; init; }
+        public int Month { get; init; }
+
+        public YearMonth(int year, int month) {
+            Year = year;
+            Month = month;
+        }
+
+        //5.1.2
+        public bool Is21Century => Year >= 2001 && Year <= 2100;
+
+        //5.1.3
+        public YearMonth AddOneMonth() {
+            var nextYear = Year;
+            var nextMonth = Month;
+            if (nextMonth == 12) {
+                nextYear += 1;
+                nextMonth = 1;
+            } else {
+                nextMonth += 1;
+            }
+            return new YearMonth(nextYear, nextMonth) ;
+        }
+
+        //5.1.4
+        public override string ToString() => Year + "年" + Month + "月";
+
+
+    }
+}
