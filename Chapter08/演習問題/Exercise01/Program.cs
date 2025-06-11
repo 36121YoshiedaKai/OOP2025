39e7e07734b01014660fa6bc160ddd1eca2b4db2
36121YoshiedaKai <oibc36121@ojs.ac.jp>
2025-06-11 10:51:49 +0900

    8.1.1完成

---
+++ Chapter08/演習問題/Exercise01/Program.cs
@@ -0,0 +1,44 @@
+﻿
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var text = "Cozy lummox gives smart squid who asks for job pen";
+
+            Exercise1(text);
+            Console.WriteLine();
+
+            Exercise2(text);
+
+        }
+
+        private static void Exercise1(string text) {
+            var dict = new Dictionary<char, int>();
+
+            foreach (var c in text.ToUpper()) {
+                if ('A' <= c && c <= 'Z') {
+                    if (dict.ContainsKey(c)) {
+                        dict[c]++;
+                    } else {
+                        dict.Add(c, 1);
+                    }
+                }
+            }
+
+            
+            foreach (var item in dict.OrderBy(n => n.Key)) {
+                Console.WriteLine($"{item.Key} : {item.Value}");
+            }
+
+
+
+
+
+
+
+        }
+
+        private static void Exercise2(string text) {
+
+        }
+    }
+}
