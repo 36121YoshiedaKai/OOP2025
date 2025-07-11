9bb87e7f85e2589605d9ed8ac970754e6a6ce6f6
36121YoshiedaKai <oibc36121@ojs.ac.jp>
2025-07-11 14:52:22 +0900

    ０１完成

---
+++ Chapter12/演習問題/Exercise01/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.Json;
+using System.Text.Unicode;
+using System.Text.Encodings.Web;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var emp = new Employee {
+                Id = 123,
+                Name = "山田太郎",
+                HireDate = new DateTime(2008, 10, 1),
+            };
+            var jsonString = Serialize(emp);
+            Console.WriteLine(jsonString);
+            var obj = Deserialize(jsonString);
+            Console.WriteLine(obj);
+
+        }
+
+
+        static string Serialize(Employee emp) {
+            var options = new JsonSerializerOptions {
+                WriteIndented = true,
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.All)
+            };
+            return JsonSerializer.Serialize(emp, options);
+        }
+
+        static Employee? Deserialize(string text) {
+            return JsonSerializer.Deserialize<Employee>(text);
+        }
+    }
+}
+
+public record Employee {
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public DateTime HireDate { get; set; }
+}
+
