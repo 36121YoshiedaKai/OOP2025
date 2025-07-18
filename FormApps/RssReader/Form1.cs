f179085d03f5ab091bc4aadf5445d826b21a0397
36121YoshiedaKai <oibc36121@ojs.ac.jp>
2025-07-18 11:17:58 +0900

    タイトル表示

---
+++ FormApps/RssReader/Form1.cs
@@ -0,0 +1,35 @@
+using System.Net;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace RssReader {
+    public partial class Form1 : Form {
+
+        private List<ItemData> items;
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void btRssGet_Click(object sender, EventArgs e) {
+            using (var wc = new WebClient()) {
+                var url = wc.OpenRead(tbUrl.Text);
+                XDocument xdoc = XDocument.Load(url);   //RSSの取得
+
+                //RSSを解析して必要な要素を取得
+                items = xdoc.Root.Descendants("item")
+                    .Select(x => new ItemData {
+                        Title = (string)x.Element("title"),
+                    }).ToList();
+
+                //リストボックスへタイトル表示
+                lbTitles.Items.Clear();
+                //foreach (var item in items) {
+                //    lbTitles.Items.Add(item.Title);
+                //}
+                items.ForEach(item => lbTitles.Items.Add(item.Title));
+
+            }
+        }
+    }
+}
