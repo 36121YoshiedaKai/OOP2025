2bf4a0157d295e90f49993fc4eec62a6d71bdbbe
36121YoshiedaKai <oibc36121@ojs.ac.jp>
2025-08-26 14:54:01 +0900

    chapter11

---
+++ Chapter11/演習問題/Exercise03/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Exercise03 {
+    internal class Program {
+        static void Main(string[] args) {
+            string[] texts = [
+                "Time is money.",
+                "What time is it?",
+                "It will take time.",
+                "We reorganized the timetable.",
+            ];
+            foreach (var line in texts) {
+                var matches = Regex.Matches(line,@"\b(time)\b",RegexOptions.IgnoreCase);
+                foreach(Match match in matches) {
+                    Console.WriteLine($"{line}:{match.Index}");
+                }
+            }
+            
+
+        }
+    }
+}
---
+++ Chapter11/演習問題/Exercise04/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.WebSockets;
+using System.Text.RegularExpressions;
+
+namespace Exercise04 {
+    internal class Program {
+        static void Main(string[] args) {
+            var lines = File.ReadAllLines("sample.txt");
+
+            var newlines = lines.Select(s => Regex.Replace(s, @"((v|V)ersion)\s*=\s*""v4.0""",@"$1=""v5.0"""));
+
+            File.WriteAllLines("sampleChange.text", newlines);
+
+            var text = File.ReadAllText("sampleChange.text");
+            Console.WriteLine(text);
+        }
+    }
+}
