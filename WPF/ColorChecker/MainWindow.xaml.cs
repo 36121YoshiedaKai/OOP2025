d3d65d08104bfd985af26b57384a4e88704fe784
36121YoshiedaKai <oibc36121@ojs.ac.jp>
2025-09-05 16:00:56 +0900

    w

---
+++ WPF/ColorChecker/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ColorChecker {
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        //すべてのスライダーから呼ばれるハンドラ
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
+
+            colorArea.Background = new SolidColorBrush(Color.FromRgb((byte)rSlider.Value, (byte)gSlider.Value, (byte)bSlider.Value));
+        }
+
+        private void stockButton_Click(object sender, RoutedEventArgs e) {
+            var getbc = colorArea.Background as SolidColorBrush;
+            bool yescolor = false;
+            if (getbc is null) {
+                return;
+            }
+
+
+            var mycolor = new MyColor {
+                Color = getbc.Color
+            };
+            foreach (var item in stocList.Items) {
+                if (item is MyColor existingMyColor) {
+                    if (existingMyColor.Color.Equals(mycolor.Color)) {
+                        yescolor = true;
+                        break;
+                    }
+                } 
+            }
+            if (!yescolor) {
+                stocList.Items.Add(mycolor);
+            }
+        }
+    }
+}
---
+++ WPF/ColorChecker/MyColor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace ColorChecker {
+    public struct MyColor {
+        public Color Color { get; set; }
+        public String Name { get; set; }
+        public override string ToString() {
+            return string.IsNullOrEmpty(Name)? $"R:{Color.R}, G:{Color.G}, B:{Color.B}": Name;
+        }
+    }
+}
