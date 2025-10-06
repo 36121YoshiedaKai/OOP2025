54809a82077d80e4ac71ca3d801c7a4099d0d55e
36121YoshiedaKai <oibc36121@ojs.ac.jp>
2025-10-06 15:59:36 +0900

    修正

--- WPF/PrismObservesSample/MainWindowViewModel.cs
+++ WPF/PrismObservesSample/MainWindowViewModel.cs
@@ -10,31 +10,37 @@ namespace PrismObservesSample {
         private string _input1 = "";
         public string Input1 {
             get => _input1;
-            set => SetProperty(ref _input1 , value );
+            set => SetProperty(ref _input1, value);
         }
 
         private string _input2 = "";
         public string Input2 {
             get => _input2;
-            set => SetProperty(ref _input2 , value);
+            set => SetProperty(ref _input2, value);
         }
 
         private string _result = "";
         public string Result {
             get => _result;
-            set => SetProperty(ref _result , value);
+            set => SetProperty(ref _result, value);
         }
 
         //コンストラクタ
         public MainWindowViewModel() {
-            SumCommand = new DelegateCommand(ExecuteSum);
+            SumCommand = new DelegateCommand(ExecuteSum, canExcuteSum)
+                .ObservesProperty(() => Input1)
+                .ObservesProperty(() => Input2);
         }
+
 
         //足し算の処理
         private void ExecuteSum() {
-            if (int.TryParse(Input1,out int i1) && int.TryParse(Input2, out int i2)) {
-                Result = (i1 + i2).ToString();
-            } 
+            Result = (int.Parse(Input1) + int.Parse(Input2)).ToString();
+        }
+
+        //足し算処理を実行できるか否かのチェック
+        private bool canExcuteSum() {
+            return int.TryParse(Input1,out _) && int.TryParse(Input2, out _);
         }
 
         public DelegateCommand SumCommand { get; }
