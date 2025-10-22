1bf36d9cbf874f39505412607293e83dd27f078d
36121YoshiedaKai <oibc36121@ojs.ac.jp>
2025-10-22 10:50:57 +0900

    ファイルの開け方を調べてる

---
+++ SQLite/CustomerApp/MainWindow.xaml.cs
@@ -0,0 +1,107 @@
+﻿using CustomerApp.Data;
+using SQLite;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CustomerApp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window {
+    private List<Customer> _customer = new List<Customer>();
+
+    public MainWindow() {
+        InitializeComponent();
+        ReadDatabese();
+    }
+
+    private void SaveButton_Click(object sender, RoutedEventArgs e) {
+        var customer = new Customer() {
+            Name = NameTextBox.Text,
+            Phone = PhoneTextBox.Text,
+            Address = AddressTextBox.Text,
+            Picture = PictureBox.,
+        };
+
+        using (var connection = new SQLiteConnection(App.databasePath)) {
+            connection.CreateTable<Customer>();
+            connection.Insert(customer);
+        }
+
+        ReadDatabese();
+
+    }
+
+    private void UpdateButton_Click(object sender, RoutedEventArgs e) {
+        var item = CustomerListView.SelectedItem as Customer;
+        if (item is null) {
+            return;
+        }
+        using (var connection = new SQLiteConnection(App.databasePath)) {
+            connection.CreateTable<Customer>();
+            var person = new Customer() {
+                Id = item.Id,
+                Name = NameTextBox.Text,
+                Phone = PhoneTextBox.Text,
+                Address = AddressTextBox.Text,
+            };
+            connection.Update(person);
+            ReadDatabese();
+
+        }
+    }
+
+    private void DeleteButton_Click(object sender, RoutedEventArgs e) {
+        var item = CustomerListView.SelectedItem as Customer;
+        if (item == null) {
+            MessageBox.Show("行を選択してください");
+            return;
+        }
+        using (var connection = new SQLiteConnection(App.databasePath)) {
+            connection.CreateTable<Customer>();
+            connection.Delete(item);        //データベースから選択されているレコードの削除
+            ReadDatabese();
+            
+
+        }
+    }
+
+    private void ReadDatabese() {
+        using (var item = new SQLiteConnection(App.databasePath)) {
+            item.CreateTable<Customer>();
+            _customer = item.Table<Customer>().ToList();
+        }
+        CustomerListView.ItemsSource = _customer;
+    }
+
+    private void SearchTextBox_TextChanged(object sender, TextChangedEventArgs e) {
+        var filterList = _customer.Where(p => p.Name.Contains(SearchTextBox.Text));
+
+        CustomerListView.ItemsSource = filterList;
+    }
+
+    private void CustomerListView_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+        var item = CustomerListView.SelectedItem as Customer;
+        if (item is null) {
+            return;
+        }
+        NameTextBox.Text = item.Name;
+        PhoneTextBox.Text = item.Phone;
+        AddressTextBox.Text = item.Address;
+    }
+
+    private void PictureButton_Click(object sender, RoutedEventArgs e) {
+        //if (.ShowDialog() == DialogResult.OK) {
+        //    PictureBox.Image = Image.FromFile(ofdPicFileOpen.FileName);
+        //}
+    }
+}
