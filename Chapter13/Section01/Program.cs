bed55da55146c86b8ceb816d80caa40d27e85e38
36121YoshiedaKai <oibc36121@ojs.ac.jp>
2025-10-27 15:28:03 +0900

    Exercise01_2

---
+++ Chapter13/Section01/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var price = Library.Books.Where(b => b.CategoryId == 1).Max(b => b.Price);
+            Console.WriteLine(price);
+
+            Console.WriteLine();
+
+            var book = Library.Books.Where(b => b.PublishedYear >= 2021).MinBy(b => b.Price);
+            Console.WriteLine(book);
+
+            Console.WriteLine();
+
+            var average = Library.Books.Average(x => x.Price);
+            var aboves = Library.Books.Where(b => b.Price > average);
+            foreach (var book1 in aboves) {
+                Console.WriteLine(book1);
+            }
+        }
+    }
+}
