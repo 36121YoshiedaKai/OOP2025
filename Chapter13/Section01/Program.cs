e22061e9f41ee237b22b9097c1914530e516bd86
36121YoshiedaKai <oibc36121@ojs.ac.jp>
2025-10-29 09:38:33 +0900

    演習問題3,4完成

--- Chapter13/Section01/Program.cs
+++ Chapter13/Section01/Program.cs
@@ -1,20 +1,27 @@
 ﻿namespace Section01 {
     internal class Program {
         static void Main(string[] args) {
-            var price = Library.Books.Where(b => b.CategoryId == 1).Max(b => b.Price);
-            Console.WriteLine(price);
+            //var price = Library.Books.Where(b => b.CategoryId == 1).Max(b => b.Price);
+            //Console.WriteLine(price);
 
-            Console.WriteLine();
+            //Console.WriteLine();
 
-            var book = Library.Books.Where(b => b.PublishedYear >= 2021).MinBy(b => b.Price);
-            Console.WriteLine(book);
+            //var book = Library.Books.Where(b => b.PublishedYear >= 2021).MinBy(b => b.Price);
+            //Console.WriteLine(book);
 
-            Console.WriteLine();
+            //Console.WriteLine();
 
-            var average = Library.Books.Average(x => x.Price);
-            var aboves = Library.Books.Where(b => b.Price > average);
-            foreach (var book1 in aboves) {
-                Console.WriteLine(book1);
+            //var average = Library.Books.Average(x => x.Price);
+            //var aboves = Library.Books.Where(b => b.Price > average);
+            //foreach (var book1 in aboves) {
+            //    Console.WriteLine(book1);
+            //}
+
+            var selected = Library.Books.GroupBy(b => b.PublishedYear)
+                .Select(group => group.MaxBy(b => b.Price))
+                .OrderBy(b => b!.PublishedYear);
+            foreach (var book in selected) {
+                Console.WriteLine($"{book!.PublishedYear}年 {book!.Title} ({book!.Price})");
             }
         }
     }
--- Chapter13/演習問題/Exercise01/Program.cs
+++ Chapter13/演習問題/Exercise01/Program.cs
@@ -25,11 +25,19 @@ namespace Exercise01 {
         }
 
         private static void Exercise1_3() {
-
+            var groups = Library.Books.GroupBy(b => b.PublishedYear).OrderBy(g => g.Key);
+            foreach (var group in groups) {
+                Console.WriteLine($"{group.Key} : {group.Count()}");
+            }
         }
 
         private static void Exercise1_4() {
-            
+            var selected = Library.Books
+                .OrderByDescending(b => b.PublishedYear)
+                .ThenByDescending(b => b.Price);
+            foreach (var book in selected) { 
+                Console.WriteLine($"{book.PublishedYear}年 {book.Price}円 {book.Title} ");
+            }
         }
 
         private static void Exercise1_5() {
