b17ffa0c6c597d70d98401057cf6bf938eb2c3fe
36121YoshiedaKai <oibc36121@ojs.ac.jp>
2025-10-30 14:52:33 +0900

    コード１４．６

---
+++ Chapter14/Section03/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace Section03 {
+    partial class Form1 {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            button1 = new Button();
+            toolStripStatuLabel1 = new StatusStrip();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(91, 71);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // toolStripStatuLabel1
+            // 
+            toolStripStatuLabel1.Location = new Point(0, 177);
+            toolStripStatuLabel1.Name = "toolStripStatuLabel1";
+            toolStripStatuLabel1.Size = new Size(488, 22);
+            toolStripStatuLabel1.TabIndex = 1;
+            toolStripStatuLabel1.Text = "statusStrip1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(488, 199);
+            Controls.Add(toolStripStatuLabel1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private StatusStrip toolStripStatuLabel1;
+    }
+}
