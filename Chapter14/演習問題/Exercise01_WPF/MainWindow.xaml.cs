756e478dd3d0438e34f8b1ff9a7ff44922ac12cf
36121YoshiedaKai <oibc36121@ojs.ac.jp>
2025-10-30 15:59:30 +0900

    ファイル読み込み

---
+++ Chapter14/演習問題/Exercise01_WPF/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Win32;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Exercise01_WPF;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window {
+    public MainWindow() {
+        InitializeComponent();
+    }
+
+    private void filebutton_Click(object sender, RoutedEventArgs e) {
+        OpenFileDialog openFileDialog = new OpenFileDialog();
+        if (openFileDialog.ShowDialog() ?? false) {
+            var filePath = openFileDialog.FileName;
+
+            try {
+                var fileContent = System.IO.File.ReadAllText(filePath);
+                filetext.Text = fileContent;
+            }
+            catch{
+            }
+        }
+    }
+}
