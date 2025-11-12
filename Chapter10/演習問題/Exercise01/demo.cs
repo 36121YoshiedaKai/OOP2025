dcbb31668e20bba23cb49b350a654fce79b7ca24
36121YoshiedaKai <oibc36121@ojs.ac.jp>
2025-11-12 10:12:56 +0900

    継承を使ったポリモーリズム

---
+++ Chapter10/演習問題/Exercise01/demo.cs
@@ -0,0 +1,24 @@
+﻿//これを10.1で使用する
+namespace Exercise01 {
+    public class EmptyLogic {
+        public class Inner {
+            public void DoNothing() {
+                int number = 42;
+                string message = "This is a meaningless string.";
+                Console.WriteLine($"{message} : {number}");
+            }
+        }
+
+        public void Start() {
+            Inner inner = new Inner();
+            inner.DoNothing();
+        }
+    }
+
+    public class AnotherEmpty {
+        public void Execute() {
+            Console.WriteLine("Executing nothing.");
+        }
+    }
+}
+
