e2579809ee1ecbcc04add8f0c1f2f77be3c576f0
36121YoshiedaKai <oibc36121@ojs.ac.jp>
2025-11-12 11:55:04 +0900

    ①完成

--- Chapter15/TemplateMethod/LineCounter/LineCounterProcessor.cs
+++ Chapter15/TemplateMethod/LineCounter/LineCounterProcessor.cs
@@ -2,18 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using TextFileProcessor;
 
 namespace LineCounter {
     internal class LineCounterProcessor : TextProcessor {
         private int _count = 0;
+        private string _text = "private";
 
         protected override void Initialize(string fname) => _count = 0;
 
-        protected override void Execute(string line) => _count++;
+        protected override void Execute(string line) {
+            if (line.Contains(_text)) {
+                _count++;
+            }
+        }
 
-        protected override void Terminate() => Console.WriteLine("{0}行", _count);
+        protected override void Terminate() => Console.WriteLine("Priveteは {0}個", _count);
     }
 
 }
