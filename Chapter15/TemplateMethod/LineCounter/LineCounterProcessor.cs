ae7a0fbc190a0a50a1f4fe071980a75b2a7d6e65
36121YoshiedaKai <oibc36121@ojs.ac.jp>
2025-11-13 13:51:10 +0900

    パス指定の単語カウント

--- Chapter15/TemplateMethod/LineCounter/LineCounterProcessor.cs
+++ Chapter15/TemplateMethod/LineCounter/LineCounterProcessor.cs
@@ -9,17 +9,29 @@ using TextFileProcessor;
 namespace LineCounter {
     internal class LineCounterProcessor : TextProcessor {
         private int _count = 0;
-        private string _text = "private";
+        private string _text = "";
 
-        protected override void Initialize(string fname) => _count = 0;
+        protected override void Initialize(string fname) {
+            _count = 0;
+            Console.Write("カウントしたい単語:");
+            _text = Console.ReadLine();
+        }
 
         protected override void Execute(string line) {
-            if (line.Contains(_text)) {
+            int index = 0;
+
+            if (string.IsNullOrWhiteSpace(_text)) {
+                Console.WriteLine("デフォルトはaです");
+                _text = "a";
+            }
+
+            while ((index = line.IndexOf(_text, index, StringComparison.OrdinalIgnoreCase)) != -1) {
                 _count++;
+                index++;
             }
         }
 
-        protected override void Terminate() => Console.WriteLine("Priveteは {0}個", _count);
+        protected override void Terminate() => Console.WriteLine($"{_text}は {_count}個");
     }
 
 }
--- Chapter15/TemplateMethod/LineCounter/Program.cs
+++ Chapter15/TemplateMethod/LineCounter/Program.cs
@@ -3,7 +3,18 @@
 namespace LineCounter {
     internal class Program {
         static void Main(string[] args) {
-            TextProcessor.Run<LineCounterProcessor>(args[0]);
+
+            
+            Console.Write("指定しいファイルを入力してください:");
+            string path = Console.ReadLine();
+
+            // @"C:\Users\infosys\source\repos\OOP2025\WPF\ConverterApp\MainWindow.xaml.cs";
+            if (!string.IsNullOrWhiteSpace(path)) {
+                
+            }else {
+                path = @"%SystemRoot%\\System32";
+            }
+                TextProcessor.Run<LineCounterProcessor>(path);
         }
     }
 }
