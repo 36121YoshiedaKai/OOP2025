c918de211d0a4797b4a41c214cd13853435d8fed
36121YoshiedaKai <oibc36121@ojs.ac.jp>
2025-11-17 15:36:37 +0900

    問題15.3完成

--- Chapter15/TemplateMethod/TextFileProcessorDI/Program.cs
+++ Chapter15/TemplateMethod/TextFileProcessorDI/Program.cs
@@ -1,7 +1,9 @@
 ﻿namespace TextFileProcessorDI {
     internal class Program {
         static void Main(string[] args) {
-            var servise = new LineCounterService();
+            //var servise = new LineCounterService();
+            //var prcessor = new TextFileProcessor(servise);
+            var servise = new LineOutputService();
             var prcessor = new TextFileProcessor(servise);
             Console.Write("ぱすの入力");
             prcessor.Run(Console.ReadLine().Replace("\"", ""));
