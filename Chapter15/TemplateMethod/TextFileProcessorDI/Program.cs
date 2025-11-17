f252a53f912048c8022648e8f7b91ae547df00b4
36121YoshiedaKai <oibc36121@ojs.ac.jp>
2025-11-17 15:05:42 +0900

    依存性の注入パターン

---
+++ Chapter15/TemplateMethod/TextFileProcessorDI/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TextFileProcessorDI {
+    internal class Program {
+        static void Main(string[] args) {
+            var servise = new LineCounterService();
+            var prcessor = new TextFileProcessor(servise);
+            Console.Write("ぱすの入力");
+            prcessor.Run(Console.ReadLine().Replace("\"", ""));
+        }
+    }
+}
---
+++ Chapter15/TemplateMethod/TextFileProcessorDI/TextFileProcessor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextFileProcessorDI {
+    public class TextFileProcessor {
+        private ITextFileService _serivce;
+
+        //コンストラクタ
+        public TextFileProcessor(ITextFileService serivce) {
+            _serivce = serivce;
+        }
+
+        public void Run(string fileName) {
+            _serivce.Initialize(fileName);
+
+            var lines = File.ReadAllLines(fileName);
+            foreach (var line in lines) {
+                _serivce.Execute(line);
+            }
+            _serivce.Terminate();
+        }
+    }
+}
