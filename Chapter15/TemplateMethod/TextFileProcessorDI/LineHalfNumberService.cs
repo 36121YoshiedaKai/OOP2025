4142d9aab025325213f8b82d2476f7929adb93d5
36121YoshiedaKai <oibc36121@ojs.ac.jp>
2025-11-19 09:58:43 +0900

    問題15.1完成

--- Chapter15/TemplateMethod/TextFileProcessorDI/LineHalfNumberService.cs
+++ Chapter15/TemplateMethod/TextFileProcessorDI/LineHalfNumberService.cs
@@ -15,40 +15,10 @@ namespace TextFileProcessorDI {
 
         public void Execute(string line) {
             foreach (char c in line) {
-                switch (c) {
-                    case '０':
-                        _sb.Append('0');
-                        break;
-                    case '１':
-                        _sb.Append('1');
-                        break;
-                    case '２':
-                        _sb.Append('2');
-                        break;
-                    case '３':
-                        _sb.Append('3');
-                        break;
-                    case '４':
-                        _sb.Append('4');
-                        break;
-                    case '５':
-                        _sb.Append('5');
-                        break;
-                    case '６':
-                        _sb.Append('6');
-                        break;
-                    case '７':
-                        _sb.Append('7');
-                        break;
-                    case '８':
-                        _sb.Append('8');
-                        break;
-                    case '９':
-                        _sb.Append('9');
-                        break;
-                    default:
-                        _sb.Append(c);
-                        break;
+                if ('０'<= c && c <= '９') {
+                    _sb.Append((char)(c - '０' + '0'));
+                } else {
+                    _sb.Append(c);
                 }
             }
             _sb.AppendLine();
